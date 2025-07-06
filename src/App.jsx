87fa3a69b47f14e124f3738c473fa0a669a6e9d5
yassineourrou <0777253724ourrou@gmail.com>
2025-07-06 21:12:50 +0100

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import Navbare from "./components/Navbare";
-import DiningEvents from "./Sections/DinerEvent";
-import Hero from "./Sections/Hero";
-import Footer from "./Sections/Footer";
-import OurStory from "./Sections/OurStory";
-import TodaySpecial from "./Sections/TodaySpecial";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import OurSpecialities from './Sections/OurSpecialities';
+import Navbare from './components/Navbare';
+import DiningEvents from './Sections/DinerEvent';
+import Hero from './Sections/Hero'
+import Footer from './Sections/Footer'
 function App() {
+
   return (
     <>
-      <Navbare />
-      <Hero />
-      <TodaySpecial />
-      {/* <OurSpecialities /> */}
-      <DiningEvents />
-      <OurStory />
-      <Footer />
+    <Navbare/>
+    <Hero/>
+    <OurSpecialities/>
+     <DiningEvents/>
+    <Footer/>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
